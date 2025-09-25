c206103cd7d78e645f9c1d2457193b256742476a
justnant <justin@beitay.com>
2025-09-24 22:07:57 -0400

    Changed some formatting

--- bad-version.py
+++ bad-version.py
@@ -5,22 +5,21 @@ print("Choose +, -, *, /")
 op = input("Op: ")
 
 if op == "+":
-    a= float(input("Number1: "))
-    b= float(input("Number2: "))
+    a= float(input("Enter num1: "))
+    b= float(input("Enter num2: "))
     print("Result:" + str(a + b))
 if op== "-":
-    x = float(input("Enter num: "))
-    y = float(input("Enter num again: "))
+    x = float(input("Enter num1: "))
+    y = float(input("Enter num2: "))
     print("Answer is " + str(x - y))
 if op == "*":
-    n1 = float(input("Enter n1: "))
-    n2 = float(input("Enter n2: "))
+    n1 = float(input("Enter num1: "))
+    n2 = float(input("Enter num2: "))
     print("Product: " + str(n1 * n2))
 if op == "/":
-    i = float(input("first: "))
-    j = float(input("second: "))
+    i = float(input("Enter num1: "))
+    j = float(input("Enter num2: "))
     if j== 0:
         print("Error, divide by zero? Really?")
     else:
         print("Quotient is " + str(i / j))
-        
